1be7918a7d688a7e79ffb4960290ff71a7561596
michaelhrivnak <mdhrivnak@gmail.com>
2020-06-09 11:35:02 -0400

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+import Intro from "../components/Intro/Intro";
+import Bio from "../components/Bio/Bio";
+import Portfolio from "../components/Portfolio/Portfolio";
+import Resume from "../components/Resume/Resume";
+import Contact from "../components/Contact/Contact";
 
 const IndexPage = () => (
   <Layout>
@@ -14,8 +19,13 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Intro/>
+    <Bio/>
+    <Skills/>
+    <Portfolio/>
+    <Resume/>
+    <Contact/>
+    <Link to="/page-2/">Go to page 2</Link> <br />    
   </Layout>
 )
 
